356ba23b4273107d2e4147776edbbb79b7be9b6d
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-20 08:09:50 +0530

    Update isAuth.js

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,19 +1,37 @@
+// import jwt from "jsonwebtoken";
+
+// const isAuth = async (req, res, next) => {
+//   try {
+//     const token = req.cookies.token;
+//     if (!token) {
+//       return res.status(400).json({ message: "token is not found" });
+//     }
+
+//     const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+//     req.userId = verifyToken.userId;
+
+//     next();
+//   } catch (error) {
+//     return res.status(500).json({ message: `is auth error ${error}` });
+//   }
+// };
+
+// export default isAuth;
+
+
 import jwt from "jsonwebtoken";
 
-const isAuth = async (req, res, next) => {
-  try {
-    const token = req.cookies.token;
-    if (!token) {
-      return res.status(400).json({ message: "token is not found" });
-    }
+export default function isAuth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
-    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = verifyToken.userId;
-
-    next();
-  } catch (error) {
-    return res.status(500).json({ message: `is auth error ${error}` });
+  if (!token) {
+    return res.status(400).json({ message: "token is not found" });
   }
-};
 
-export default isAuth;
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: "invalid token" });
+    req.user = user;
+    next();
+  });
+}
