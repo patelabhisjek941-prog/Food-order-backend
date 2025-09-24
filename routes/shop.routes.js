33bee177600c1743dae3f2e9cc2d6e8d9f8a7af0
patelabhisjek941-prog <patelabhisjek941@gmail.com>
2025-09-24 19:36:12 +0530

    Update shop.routes.js

--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -9,7 +9,8 @@ const shopRouter=express.Router()
 
 shopRouter.get("/getall",isAuth,getAllShops)
 shopRouter.get("/getcurrent",isAuth,getCurrentShop)
-shopRouter.post("/editshop",isAuth,upload.single("image"),addShop)
+shopRouter.post("/editshop",isAuth,upload.single("image"),editShop)
+// shopRouter.post("/editshop",isAuth,upload.single("image"),addShop)
 shopRouter.get("/getshopsbycity/:city",isAuth,getShopsByCity)
 shopRouter.get("/getshopbyid/:shopId",isAuth,getShopById)
 export default shopRouter
